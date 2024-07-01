536c6fb5102e0ff31f367bf0263e74ba2da954bf
Krezh <krezh@users.noreply.github.com>
2024-07-01 15:24:11 +0200

    chore: Add Prometheus exporter user to RouterOS configuration

--- infrastructure/terraform/routeros/main.tf
+++ infrastructure/terraform/routeros/main.tf
@@ -7,3 +7,17 @@ data "doppler_secrets" "prd_routeros" {}
 resource "routeros_system_identity" "identity" {
   name = "yggdrasil"
 }
+
+resource "routeros_system_user_group" "prometheus_exporter" {
+  name   = "prometheus_exporter"
+  policy = ["api", "!ftp", "!local", "!password", "!policy", "read", "!reboot", "!rest-api", "!romon", "!sensitive", "!sniff", "!ssh", "!telnet", "!test", "!web", "!winbox", "!write"]
+}
+
+resource "routeros_system_user" "prometheus_exporter" {
+  depends_on = [routeros_system_user_group.prometheus_exporter]
+  name       = "prometheus_exporter"
+  address    = "0.0.0.0/0"
+  group      = "prometheus_exporter"
+  password   = data.doppler_secrets.prd_routeros.map.PROMETHEUS_EXPORTER
+  comment    = var.comment
+}
