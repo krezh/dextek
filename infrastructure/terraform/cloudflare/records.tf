9e964e2897072cab335f6e30035af0117ef45027
Krezh <krezh@users.noreply.github.com>
2024-08-10 01:15:40 +0200

    chore: update cloudflare record values in Terraform

--- infrastructure/terraform/cloudflare/records.tf
+++ infrastructure/terraform/cloudflare/records.tf
@@ -2,7 +2,7 @@ resource "cloudflare_record" "ipv4" {
   for_each = toset(var.domains)
   name     = "ingress"
   zone_id  = data.cloudflare_zone.domain[each.key].id
-  value    = chomp(data.http.ipv4.response_body)
+  content  = chomp(data.http.ipv4.response_body)
   proxied  = true
   type     = "A"
   ttl      = 1
@@ -12,6 +12,6 @@ resource "cloudflare_record" "doppler" {
   for_each = toset(var.domains)
   name     = "_doppler_O9Zugt4K0nNkt"
   zone_id  = data.cloudflare_zone.domain[each.key].id
-  value    = "5d0SML7ayFMKTmY18mJ6FQI3L1HB4EFq"
+  content  = "5d0SML7ayFMKTmY18mJ6FQI3L1HB4EFq"
   type     = "TXT"
 }
