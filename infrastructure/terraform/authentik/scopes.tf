58c90a2fd6fa261827c831cd8ff0a7cc343d33bb
Krezh <krezh@noreply.codeberg.org>
2025-01-14 01:24:33 +0100

    Authentik-terraform (#1778)
    * first commit
    
    * cleanup
    
    * add terraform for authentik and migrate
    
    ---------
    
    Co-authored-by: Krezh <krezh@users.noreply.github.com>

---
+++ infrastructure/terraform/authentik/scopes.tf
@@ -0,0 +1,8 @@
+## OAuth scopes
+data "authentik_property_mapping_provider_scope" "oauth2" {
+  managed_list = [
+    "goauthentik.io/providers/oauth2/scope-openid",
+    "goauthentik.io/providers/oauth2/scope-email",
+    "goauthentik.io/providers/oauth2/scope-profile"
+  ]
+}
