dd8865bc1e0de10809e073add59877612ca28f54
Krezh <krezh@noreply.codeberg.org>
2025-01-23 17:14:54 +0100

    fix plex source

--- infrastructure/terraform/authentik/flows.tf
+++ infrastructure/terraform/authentik/flows.tf
@@ -2,6 +2,10 @@ data "authentik_flow" "default-source-authentication" {
   slug = "default-source-authentication"
 }
 
+data "authentik_flow" "default-source-enrollment" {
+  slug = "default-source-enrollment"
+}
+
 data "authentik_flow" "default-provider-authorization-implicit-consent" {
   slug = "default-provider-authorization-implicit-consent"
 }
