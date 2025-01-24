21b7f778cacc522846a588905c6b87f1c1a27804
Krezh <krezh@noreply.codeberg.org>
2025-01-24 23:14:11 +0100

    remove vlan 25

--- infrastructure/terraform/routeros/bridge_vlans.tf
+++ infrastructure/terraform/routeros/bridge_vlans.tf
@@ -22,14 +22,6 @@ resource "routeros_interface_bridge_vlan" "vlan20" {
   comment  = "K8S: ${var.comment}"
 }
 
-resource "routeros_interface_bridge_vlan" "vlan25" {
-  bridge   = routeros_interface_bridge.bridge.name
-  vlan_ids = [25]
-  tagged   = ["bridge"]
-  untagged = []
-  comment  = "K8S BGP: ${var.comment}"
-}
-
 resource "routeros_interface_bridge_vlan" "vlan50" {
   bridge   = routeros_interface_bridge.bridge.name
   vlan_ids = [50]
@@ -43,9 +35,6 @@ resource "routeros_interface_bridge_vlan" "vlan100" {
   vlan_ids = [100]
   tagged = [
     "bridge", "ether1",
-    routeros_interface_bridge_port.asgard_eth0.interface,
-    routeros_interface_bridge_port.vanaheim_eth0.interface,
-    routeros_interface_bridge_port.alfheim_eth0.interface,
     routeros_interface_bridge_port.ms01-01_eth0.interface,
     routeros_interface_bridge_port.ms01-02_eth0.interface,
     routeros_interface_bridge_port.ms01-03_eth0.interface,
