427a7d6da18650065b58dd3996f1605172ecd8f8
Krezh <krezh@noreply.codeberg.org>
2025-02-17 15:33:26 +0100

    feat(authentik): update zipline OAuth2 scopes and mappings for offline access

--- infrastructure/terraform/authentik/scopes.tf
+++ infrastructure/terraform/authentik/scopes.tf
@@ -6,3 +6,12 @@ data "authentik_property_mapping_provider_scope" "oauth2" {
     "goauthentik.io/providers/oauth2/scope-profile"
   ]
 }
+
+data "authentik_property_mapping_provider_scope" "oauth2_offline_access" {
+  managed_list = [
+    "goauthentik.io/providers/oauth2/scope-openid",
+    "goauthentik.io/providers/oauth2/scope-email",
+    "goauthentik.io/providers/oauth2/scope-profile",
+    "goauthentik.io/providers/oauth2/scope-offline_access"
+  ]
+}
