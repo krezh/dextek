7c8cc589ac822541c92cfdb3edba09cce22a36ca
Krezh <krezh@users.noreply.github.com>
2025-02-22 18:34:11 +0100

    fix(routeros): change MTU from 9000 to 1500 and remove jumbo frame options

--- infrastructure/terraform/routeros/interface_vlans.tf
+++ infrastructure/terraform/routeros/interface_vlans.tf
@@ -2,5 +2,5 @@ resource "routeros_interface_vlan" "vlan200" {
   interface = routeros_interface_bridge.bridge.name
   name      = "vlan200"
   vlan_id   = 200
-  mtu       = 9000
+  mtu       = 1500
 }
