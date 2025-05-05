b8a51358d4c2bd161934a226180e2bd71daa39c7
Krezh <krezh@noreply.codeberg.org>
2025-05-05 14:13:57 +0200

    feat(authentik): add custom CSS for Plexuz branding

--- infrastructure/terraform/authentik/system.tf
+++ infrastructure/terraform/authentik/system.tf
@@ -3,10 +3,11 @@ data "authentik_certificate_key_pair" "generated" {
 }
 
 resource "authentik_brand" "plexuz" {
-  domain           = var.domain
-  branding_title   = "Plexuz"
-  branding_logo    = "/static/dist/assets/icons/icon_left_brand.svg"
-  branding_favicon = "/static/dist/assets/icons/icon.png"
+  domain              = var.domain
+  branding_title      = "Plexuz"
+  branding_logo       = "/static/dist/assets/icons/icon_left_brand.svg"
+  branding_favicon    = "/static/dist/assets/icons/icon.png"
+  branding_custom_css = file("branding/plexuz.css")
 
   flow_authentication = authentik_flow.authentication.uuid
   flow_invalidation   = authentik_flow.invalidation.uuid
