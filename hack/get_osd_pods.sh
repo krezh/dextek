bcb25739ea774352d060c40ba1be9d414dbd34c1
Krezh <krezh@noreply.codeberg.org>
2025-07-28 18:57:36 +0200

    refactor(taskfiles): remove Bootstrap taskfile and clean up scripts

--- hack/get_osd_pods.sh
+++ hack/get_osd_pods.sh
@@ -8,14 +8,14 @@ OSD_PODS=$(kubectl get pods --all-namespaces -l \
 # Find node and drive associations from OSD pods
 for pod in ${OSD_PODS}
 do
- echo "Pod:  ${pod}"
- echo "Node: $(kubectl -n rook-ceph get pod "${pod}" -o jsonpath='{.spec.nodeName}')"
- kubectl -n rook-ceph exec "${pod}" -- sh -c '\
-  for i in /var/lib/ceph/osd/ceph-*; do
-    [ -f ${i}/ready ] || continue
-    echo -ne "-$(basename ${i}) "
-    echo $(lsblk -n -o NAME,SIZE ${i}/block 2> /dev/null || \
-    findmnt -n -v -o SOURCE,SIZE -T ${i}) $(cat ${i}/type)
-  done | sort -V
-  echo'
+  echo "Pod:  ${pod}"
+  echo "Node: $(kubectl -n rook-ceph get pod "${pod}" -o jsonpath='{.spec.nodeName}')"
+  kubectl -n rook-ceph exec "${pod}" -c osd -- sh -c '\
+    for i in /var/lib/ceph/osd/ceph-*; do
+      [ -f ${i}/ready ] || continue
+      echo -ne "-$(basename ${i}) "
+      echo $(lsblk -n -o NAME,SIZE ${i}/block 2> /dev/null || \
+      findmnt -n -v -o SOURCE,SIZE -T ${i}) $(cat ${i}/type)
+    done | sort -V
+    echo'
 done
