146ba7b330c6967f7a0be8b72d8abc4e48a8fd3a
Krezh <krezh@users.noreply.github.com>
2025-10-11 01:36:38 +0200

    Remove unused and null outpost config options

--- infrastructure/terraform/authentik/outposts.tf
+++ infrastructure/terraform/authentik/outposts.tf
@@ -7,22 +7,12 @@ resource "authentik_outpost" "internal" {
     "log_level"                      = "info"
     "authentik_host"                 = "https://sso.${var.domain["external"]}/"
     "authentik_host_insecure"        = false
-    "authentik_host_browser"         = ""
     "refresh_interval"               = "minutes=5"
-    "docker_labels"                  = null
-    "docker_network"                 = null
-    "docker_map_ports"               = true
-    "container_image"                = null
     "kubernetes_replicas"            = 1
     "kubernetes_namespace"           = "auth"
     "object_naming_template"         = "ak-outpost-%(name)s"
-    "kubernetes_json_patches"        = null
     "kubernetes_service_type"        = "ClusterIP"
-    "kubernetes_image_pull_secrets"  = []
-    "kubernetes_ingress_class_name"  = "nginx-internal"
     "kubernetes_disabled_components" = ["ingress", "traefik middleware"]
-    "kubernetes_ingress_annotations" = {}
-    "kubernetes_ingress_secret_name" = "authentik-outpost-tls"
     "kubernetes_httproute_parent_refs" = [
       {
         name        = "gateway-internal"
@@ -41,22 +31,12 @@ resource "authentik_outpost" "external" {
     "log_level"                      = "info"
     "authentik_host"                 = "https://sso.${var.domain["external"]}/"
     "authentik_host_insecure"        = false
-    "authentik_host_browser"         = ""
     "refresh_interval"               = "minutes=5"
-    "docker_labels"                  = null
-    "docker_network"                 = null
-    "docker_map_ports"               = true
-    "container_image"                = null
     "kubernetes_replicas"            = 1
     "kubernetes_namespace"           = "auth"
     "object_naming_template"         = "ak-outpost-%(name)s"
-    "kubernetes_json_patches"        = null
     "kubernetes_service_type"        = "ClusterIP"
-    "kubernetes_image_pull_secrets"  = []
-    "kubernetes_ingress_class_name"  = "nginx-external"
     "kubernetes_disabled_components" = ["ingress", "traefik middleware"]
-    "kubernetes_ingress_annotations" = {}
-    "kubernetes_ingress_secret_name" = "authentik-outpost-tls"
     "kubernetes_httproute_parent_refs" = [
       {
         name        = "gateway-external"
