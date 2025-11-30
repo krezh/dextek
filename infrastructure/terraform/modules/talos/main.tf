b304ae93c3c9bb5dea4b43519b60a52ccd9b9fd0
Krezh <krezh@users.noreply.github.com>
2025-11-30 02:13:08 +0100

    test

--- infrastructure/terraform/modules/talos/main.tf
+++ infrastructure/terraform/modules/talos/main.tf
@@ -32,6 +32,7 @@ data "talos_machine_configuration" "machine" {
       hostname           = each.key,
       disk_model         = each.value.disk_model,
       mac_addr           = each.value.mac_addr,
+      mac_addr_10g       = each.value.mac_addr_10g,
       cluster_vip        = var.cluster_vip,
       driver             = each.value.driver,
       driver_10g         = each.value.driver_10g,
