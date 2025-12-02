36d4f7774cf8a45f798d3ebed39f4d7b3523b6e8
Krezh <krezh@noreply.codeberg.org>
2025-12-02 04:58:54 +0100

    test

--- infrastructure/terraform/routeros/interface_bridge.tf
+++ infrastructure/terraform/routeros/interface_bridge.tf
@@ -1,7 +1,7 @@
 resource "routeros_interface_bridge" "bridge" {
   name              = "bridge"
   vlan_filtering    = true
-  protocol_mode     = "rstp"
-  ingress_filtering = false
+  protocol_mode     = "none"
+  ingress_filtering = var.ingress_filtering
   comment           = "Default Bridge: ${var.comment}"
 }
