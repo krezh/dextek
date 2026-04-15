18b2734a4d52a068a73c8307c8ee4c707cb47a2f
Krezh <krezh@noreply.codeberg.org>
2026-04-15 03:07:21 +0200

    fix

--- infrastructure/terraform/hetzner/infisical.tf
+++ infrastructure/terraform/hetzner/infisical.tf
@@ -10,6 +10,13 @@ ephemeral "infisical_secret" "hcloud_token" {
   folder_path  = "/Terraform/Hetzner"
 }
 
+ephemeral "infisical_secret" "ssh_private_key" {
+  name         = "SSH_PRIVATE_KEY"
+  env_slug     = local.infisical_env
+  workspace_id = local.infisical_project_id
+  folder_path  = "/Terraform/Hetzner"
+}
+
 data "infisical_secrets" "pangolin" {
   env_slug     = local.infisical_env
   workspace_id = local.infisical_project_id
