c4cfe84662c9a2c029073181dff9abe26e95f4f3
Krezh <krezh@noreply.codeberg.org>
2026-04-15 02:50:28 +0200

    test

--- infrastructure/terraform/authentik/providers.tf
+++ infrastructure/terraform/authentik/providers.tf
@@ -30,12 +30,6 @@ data "sops_file" "secrets" {
 
 provider "infisical" {
   host = "https://eu.infisical.com"
-  auth = {
-    universal = {
-      client_id     = var.infisical_client_id
-      client_secret = var.infisical_client_secret
-    }
-  }
 }
 
 provider "authentik" {
