f13144d922a262a9e86a3d8bb4bb87619d9b7900
Krezh <krezh@noreply.codeberg.org>
2026-04-15 02:56:17 +0200

    test

--- infrastructure/terraform/authentik/providers.tf
+++ infrastructure/terraform/authentik/providers.tf
@@ -11,21 +11,11 @@ terraform {
       source  = "goauthentik/authentik"
       version = "2025.12.1"
     }
-    sops = {
-      source  = "carlpett/sops"
-      version = "1.4.1"
-    }
     infisical = {
       source  = "Infisical/infisical"
       version = "0.16.15"
     }
   }
-}
-
-provider "sops" {}
-
-data "sops_file" "secrets" {
-  source_file = "secret.sops.yaml"
 }
 
 provider "infisical" {
