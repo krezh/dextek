fbcdf5bac1c444755150e68c98b77e1ffd0cf81a
Krezh <krezh@noreply.codeberg.org>
2026-04-15 03:32:56 +0200

    test

--- infrastructure/terraform/hetzner/providers.tf
+++ infrastructure/terraform/hetzner/providers.tf
@@ -23,6 +23,10 @@ terraform {
       source  = "loafoe/ssh"
       version = "2.7.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.3"
+    }
   }
 }
 
@@ -34,8 +38,14 @@ provider "hcloud" {
   token = ephemeral.infisical_secret.hcloud_token.value
 }
 
+resource "local_sensitive_file" "ssh_key" {
+  content         = local.ssh_key
+  filename        = "/tmp/tofu_ssh_key"
+  file_permission = "0600"
+}
+
 provider "docker" {
   host     = "ssh://${var.ssh_user}@${hcloud_server.pangolin.ipv4_address}"
-  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
+  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null", "-i", local_sensitive_file.ssh_key.filename]
 }
 
