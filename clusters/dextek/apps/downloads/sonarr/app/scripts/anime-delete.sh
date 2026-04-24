d9d952bb807de1b23d00a4ea2c32f32c5dbd270d
Krezh <krezh@noreply.codeberg.org>
2026-04-24 14:58:18 +0200

    remove sonarr script

--- clusters/dextek/apps/downloads/sonarr/app/scripts/anime-delete.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-locations=(
-	"/downloads/complete"
-)
-
-find "${locations[@]}" -type f \( -iname "*op[0-9]*" -o -iname "*nced*" -o -iname "*ncop*" -o -iname "*music video*" \) -exec rm -rf {} \;
