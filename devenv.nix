f9677dec1e00ad1ed7a3b78b587379dccf3a2873
Krezh <krezh@users.noreply.github.com>
2026-05-21 16:40:57 +0200

    fix bootstrap and add more dependson

---
+++ devenv.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }: {
+  packages = [
+    pkgs.pulumi-bin
+    pkgs.pulumiPackages.pulumi-go
+    pkgs.infisical
+    pkgs.go
+  ];
+
+  enterShell = ''
+    eval $(infisical export --format=dotenv-export --silent --path=/Kubernetes/DexTek/TFController --env=default)
+
+    export INFISICAL_UNIVERSAL_AUTH_CLIENT_ID=$INFISICAL_ID
+    echo INFISICAL_UNIVERSAL_AUTH_CLIENT_ID exported
+    export INFISICAL_UNIVERSAL_AUTH_CLIENT_SECRET=$INFISICAL_SECRET
+    echo INFISICAL_UNIVERSAL_AUTH_CLIENT_SECRET exported
+  '';
+}
