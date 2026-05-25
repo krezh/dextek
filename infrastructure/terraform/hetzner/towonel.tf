a50fb1ef6d6eff5413f561411b8afe196efbe147
Krezh <krezh@users.noreply.github.com>
2026-05-25 23:45:54 +0200

    Revert "update towonel"
    This reverts commit 3aa7f898070bd44b44179232086a2e0838626343.

--- infrastructure/terraform/hetzner/towonel.tf
+++ infrastructure/terraform/hetzner/towonel.tf
@@ -7,7 +7,7 @@ resource "docker_network" "edge" {
 
 resource "docker_image" "towonel" {
   depends_on = [ssh_resource.docker_tls_setup]
-  name       = "codeberg.org/towonel/towonel-node:0.1.3"
+  name       = "git.erwanleboucher.dev/eleboucher/towonel-node:0.0.43"
 }
 
 resource "docker_container" "towonel" {
