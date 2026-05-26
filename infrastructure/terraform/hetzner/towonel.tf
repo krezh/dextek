6a6f98d224cc7a52198c8f3f9654dcb990f5519f
Krezh <krezh@users.noreply.github.com>
2026-05-26 18:26:15 +0200

    fix

--- infrastructure/terraform/hetzner/towonel.tf
+++ infrastructure/terraform/hetzner/towonel.tf
@@ -26,7 +26,7 @@ resource "docker_container" "towonel" {
     "TOWONEL_INVITE_HASH_KEY=${data.infisical_secrets.towonel.secrets["TOWONEL_INVITE_HASH_KEY"].value}",
     "TOWONEL_HUB_ENABLED=true",
     "TOWONEL_HUB_ALLOW_PRIVILEGED_PORTS=true",
-    "TOWONEL_HUB_PUBLIC_URL=https://towonel.plexuz.xyz",
+    "TOWONEL_HUB_PUBLIC_URL=https://twnl.plexuz.xyz",
     "TOWONEL_EDGE_ENABLED=true",
     "TOWONEL_EDGE_HEALTH_LISTEN_ADDR=0.0.0.0:9092",
     "TOWONEL_EDGE_IROH_PORT=51820",
