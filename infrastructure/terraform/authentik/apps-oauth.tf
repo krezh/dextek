59a4854f0764f39c9601a484b1c0b0212e8a41ef
Krezh <17014764+krezh@users.noreply.github.com>
2026-09-09 12:24:35 +0200

    Remove 'papra' app configuration from apps-oauth.tf
    Removed the 'papra' application configuration from OAuth settings.
    
    Signed-off-by: Krezh <17014764+krezh@users.noreply.github.com>

--- infrastructure/terraform/authentik/apps-oauth.tf
+++ infrastructure/terraform/authentik/apps-oauth.tf
@@ -102,16 +102,6 @@ module "oauth_apps" {
       redirect_uri_paths = ["/auth/oidc/callback"]
       meta_description   = ""
     }
-    papra = {
-      app_group = "Tools"
-      access_groups = [
-        data.authentik_group.superuser.id,
-        authentik_group.groups["users"].id
-      ]
-      client_id          = data.infisical_secrets.papra.secrets["PAPRA_OAUTH_CLIENT_ID"].value
-      client_secret      = data.infisical_secrets.papra.secrets["PAPRA_OAUTH_CLIENT_SECRET"].value
-      redirect_uri_paths = ["/api/auth/oauth2/callback/authentik"]
-    }
     qui = {
       app_group = "Downloads"
       access_groups = [
